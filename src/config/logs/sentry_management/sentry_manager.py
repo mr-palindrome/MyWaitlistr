8d03b7160e045ac105d7e244911d32fac5910008
Nayan Das <ndas5144@gmail.com>
2024-05-09 01:37:06 +0530

    chore: fixed sentry initialization issue

--- src/config/logs/sentry_management/sentry_manager.py
+++ src/config/logs/sentry_management/sentry_manager.py
@@ -3,7 +3,7 @@ import sentry_sdk
 from src.config.settings import SENTRY_DSN
 
 
-async def initialize_sentry():
+def initialize_sentry():
     sentry_sdk.init(
         dsn=SENTRY_DSN,
         # Set traces_sample_rate to 1.0 to capture 100%
